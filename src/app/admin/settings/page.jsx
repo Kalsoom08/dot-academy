871c0b49f4f5c8cbf8148e3342f1eb5c837633af
Basirat Hamayoon <basirathumayun@gmail.com>
2025-08-01 11:58:46 +0500

    Create settings componenet for admin module

---
+++ src/app/admin/settings/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminSettings from './adminSettings.jsx/page'
+
+function page() {
+  return (
+    <div>
+       <AdminSettings />
+    </div>
+  )
+}
+
+export default page
