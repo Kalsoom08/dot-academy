b46a85a2085f5402c54a4ac196897cfd4748c8cd
Basirat Hamayoon <basirathumayun@gmail.com>
2025-09-29 12:37:55 +0500

    Updationg the course problems

--- src/app/AuthComponents/ExploreCourses/page.jsx
+++ src/app/AuthComponents/ExploreCourses/page.jsx
@@ -3,6 +3,7 @@
 import Footer from '../../../components/AuthFooter';
 import Explore from '../ExploreCourses/explore';
 import QuizQuestionCard from '../../../components/Quiz/QuizQuestionCard';
+import Courses from '../ExploreCourses/Courses';
 
 export default function ExploreCoursesPage() {
   
@@ -11,7 +12,7 @@ export default function ExploreCoursesPage() {
       <div className="flex h-screen flex-col">
      
           <main className="flex-1">
-         <Explore />
+         <Courses />
 
           </main>
        
